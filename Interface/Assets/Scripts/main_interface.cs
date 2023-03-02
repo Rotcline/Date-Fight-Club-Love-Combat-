deef3c73fc4375d94b364b0d57b50b8466bb58dd
CarlosV27 <a00832076@tec.mx>
2023-03-01 23:34:55 -0600

    Background adn skybox

--- Interface/Assets/Scripts/main_interface.cs
+++ Interface/Assets/Scripts/main_interface.cs
@@ -12,40 +12,40 @@ public class main_interface : MonoBehaviour
     public TMP_InputField inputPrompt;
     private Queue listOfMovements = new Queue();
     
-    static async Task Function()
-    {
-        using var httpClient = new HttpClient();
+    //static async Task Function()
+    //{
+    //    using var httpClient = new HttpClient();
 
-        var url = "https://randomuser.me/api/";
+    //    var url = "https://randomuser.me/api/";
 
-        var response = await httpClient.GetAsync(url);
+    //    var response = await httpClient.GetAsync(url);
 
-        if (response.IsSuccessStatusCode)
-        {
-            var content = await response.Content.ReadAsStringAsync();
-            var json_complete = JsonConvert.DeserializeObject<MessageResponse>(content);
-            Debug.Log($"");
-        }
-    }
+    //    if (response.IsSuccessStatusCode)
+    //    {
+    //        var content = await response.Content.ReadAsStringAsync();
+    //        var json_complete = JsonConvert.DeserializeObject<MessageResponse>(content);
+    //        Debug.Log($"");
+    //    }
+    //}
 
     public void SubmitText()
     {
         Debug.Log(inputPrompt.text);
         inputPrompt.text = "";
         inputPrompt.placeholder.gameObject.SetActive(false);
-        Function();
+        //Function();
     }
 
-    public class MessageResponse
-    {
-        public string emotion { get; set; }
-        public string message { get; set; }
+    //public class MessageResponse
+    //{
+    //    public string emotion { get; set; }
+    //    public string message { get; set; }
         
-    }
+    //}
 
-    public class Test1
-    {
-        public int counter { get; set; }
-        public List<String> some_shit { get; set; }
-    }
+    //public class Test1
+    //{
+    //    public int counter { get; set; }
+    //    public List<String> some_shit { get; set; }
+    //}
 }
