aed794d1b1e2db8494954e5bc866aafd36b107f8
korcline@hotmail.com <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>
2023-03-03 11:32:19 -0600

    Add full animation control

--- CyberBusters/Assets/Scripts/HoldFightController.cs
+++ CyberBusters/Assets/Scripts/HoldFightController.cs
@@ -6,6 +6,7 @@ public class HoldFightController : MonoBehaviour
 {
     private Animator anim;
     private float holdTime;
+    public float speed=1;
 
     // Start is called before the first frame update
     void Start()
@@ -16,31 +17,105 @@ public class HoldFightController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f & anim.GetCurrentAnimatorStateInfo(0).IsName("StrongSwordMid"))
-        {
-            anim.SetBool("StrongSwordMid", false);
+        if (Input.GetKey(KeyCode.D)){
+            anim.SetBool("Running",true);
+            transform.rotation = Quaternion.Euler(0,90,0);
+            transform.Translate( Vector3.forward * speed * Time.deltaTime);
         }
-        else if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.7f)
-        {
-            anim.SetBool("MediumSwordMid", false);
-            anim.SetBool("LowSwordMid", false);
+        else if (Input.GetKey(KeyCode.A)){
+            anim.SetBool("Running",true);
+            transform.rotation = Quaternion.Euler(0,270,0);
+            transform.Translate( Vector3.forward* speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.W)){
+            anim.SetBool("Running",true);
+            transform.rotation = Quaternion.Euler(0,180,0);
+            transform.Translate( Vector3.forward * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S)){
+            anim.SetBool("Running", true);
+            transform.rotation = Quaternion.Euler(0,0,0);
+            transform.Translate( Vector3.forward* speed * Time.deltaTime);
         }
 
-        if (Input.GetKeyDown(KeyCode.L))
+        if (!Input.GetKey(KeyCode.D) & !Input.GetKey(KeyCode.A) & !Input.GetKey(KeyCode.W) & !Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("Running", false);
+        }
+        stopAnim("StrongSwordMid", 1f);
+        stopAnim("MediumSwordMid", 0.7f);
+        stopAnim("LowSwordMid", 0.7f);
+
+        stopAnim("StrongSwordLow", 1f);
+        stopAnim("MediumSwordLow", 0.7f);
+        stopAnim("LowSwordLow", 0.7f);
+
+        stopAnim("StrongSwordHigh", 1f);
+        stopAnim("MediumSwordHigh", 0.7f);
+        stopAnim("LowSwordHigh", 0.7f);
+
+        stopAnim("StrongKickMid", 1f);
+        stopAnim("MediumKickMid", 0.7f);
+        stopAnim("LowKickMid", 0.7f);
+
+        stopAnim("StrongKickLow", 1f);
+        stopAnim("MediumKickLow", 0.7f);
+        stopAnim("LowKickLow", 0.7f);
+
+        stopAnim("StrongKickHigh", 1f);
+        stopAnim("MediumKickHigh", 0.7f);
+        stopAnim("LowKickHigh", 0.7f);
+
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            attackWithForce("KickLow", KeyCode.K);
+            attackWithForce("SwordLow", KeyCode.L);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            attackWithForce("KickHigh", KeyCode.K);
+            attackWithForce("SwordHigh", KeyCode.L);
+        }
+        else 
+        {
+            attackWithForce("SwordMid", KeyCode.L);
+            attackWithForce("KickMid", KeyCode.K);
+        }
+
+    }
+
+    void stopAnim(string animName, float timeToAnim)
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= timeToAnim &
+            anim.GetCurrentAnimatorStateInfo(0).IsName(animName))
+        {
+            anim.SetBool(animName, false);
+        }
+    }
+
+    void attackWithForce(string animName, KeyCode kc)
+    {
+        if (Input.GetKeyDown(kc))
         {
             holdTime = Time.time;
         }
-        if (Input.GetKeyUp(KeyCode.L) & (Mathf.Abs(holdTime-Time.time)>0 & Mathf.Abs(holdTime-Time.time)<0.1))
+        if (Input.GetKeyUp(kc))
         {
-            anim.SetBool("LowSwordMid", true);
-        }
-        else if (Input.GetKeyUp(KeyCode.L) & (Mathf.Abs(holdTime-Time.time)>0.1 & Mathf.Abs(holdTime-Time.time)<0.6))
-        {
-            anim.SetBool("MediumSwordMid", true);
-        }
-        else if (Input.GetKeyUp(KeyCode.L) & (Mathf.Abs(holdTime-Time.time)>0.6))
-        {
-            anim.SetBool("StrongSwordMid", true);
+            float timeHeld = Mathf.Abs(holdTime - Time.time);
+            
+            if (timeHeld > 0 & timeHeld < 0.1)
+            {
+                anim.SetBool("Low" + animName, true);
+            }
+            else if (timeHeld > 0.1 & timeHeld < 0.6)
+            {
+                anim.SetBool("Medium" + animName, true);
+            }
+            else if (timeHeld > 0.6)
+            {
+                anim.SetBool("Strong" + animName, true);
+            }
         }
     }
 }
